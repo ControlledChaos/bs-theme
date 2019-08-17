131441118a519b11f6e89cadcb9bae12ffcb3b36
ControlledChaos <greg@ccdzine.com>
2019-08-17 11:31:17 -0800

    Added SVG hamburger icon to menu toggle.

--- header.php
+++ header.php
@@ -48,7 +48,10 @@ if ( is_home() && ! is_front_page() ) {
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'bs-theme' ); ?></a>
 
 	<nav id="site-navigation" class="main-navigation" role="directory" itemscope itemtype="http://schema.org/SiteNavigationElement">
-		<button class="menu-toggle" aria-controls="main-menu" aria-expanded="false"><?php esc_html_e( 'Menu', 'bs-theme' ); ?></button>
+		<button class="menu-toggle" aria-controls="main-menu" aria-expanded="false">
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"/></svg>
+			<?php esc_html_e( 'Menu', 'bs-theme' ); ?>
+		</button>
 		<?php
 		wp_nav_menu( array(
 			'theme_location' => 'main',
