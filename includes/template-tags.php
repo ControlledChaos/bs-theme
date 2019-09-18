003589c9a7396930fe5810f6f47fea4d1cfd66b0
ControlledChaos <greg@ccdzine.com>
2019-09-18 08:37:47 -0800

    Minor text edit.
    Added space between single categories and edit link.

--- includes/template-tags.php
+++ includes/template-tags.php
@@ -242,7 +242,7 @@ function entry_footer() {
 	edit_post_link(
 		sprintf(
 			wp_kses(
-				__( 'Edit <span class="screen-reader-text">%s</span>', 'bs-theme' ),
+				__( ' Edit <span class="screen-reader-text">%s</span>', 'bs-theme' ),
 				array(
 					'span' => array(
 						'class' => array(),
