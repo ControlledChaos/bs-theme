93751839d12e50523c79369fe8e394e45ef7b001
ControlledChaos <greg@ccdzine.com>
2021-05-09 19:44:38 -0700

    Duplicated index.php as home.php

---
+++ home.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * The main blog template file
+ *
+ * @package    BS_Theme
+ * @subpackage Templates
+ * @category   Archives
+ * @since      1.0.0
+ */
+
+namespace BS_Theme;
+
+// Alias namespaces.
+use BS_Theme\Classes\Front as Front;
+
+// Get the default header file.
+get_header();
+
+?>
+	<div id="primary" class="content-area">
+
+		<main id="main" class="site-main" itemscope itemprop="mainContentOfPage">
+
+		<?php if ( have_posts() ) :
+
+			if ( is_home() && ! is_front_page() ) :
+				?>
+				<header>
+					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+				</header>
+				<?php
+			endif;
+
+			while ( have_posts() ) :
+				the_post();
+
+				get_template_part( 'template-parts/content', get_post_type() );
+
+			endwhile;
+
+			the_posts_navigation();
+
+		else :
+
+			get_template_part( 'template-parts/content', 'none' );
+
+		endif; ?>
+
+		</main>
+	</div>
+<?php
+
+// Get the default sidebar file.
+get_sidebar();
+
+// Get the default footer file.
+get_footer();
