78e6b0734fa1ae028b4b07188840fbcbc62a7676
ControlledChaos <greg@ccdzine.com>
2021-06-07 00:45:17 -0700

    Schema attribute in <html>.

--- header.php
+++ header.php
@@ -33,7 +33,7 @@ if ( is_home() && ! is_front_page() ) {
 // Hook for ACF forms & similar.
 do_action( 'before_html' ); ?>
 
-<html <?php language_attributes(); ?> class="no-js">
+<html xmlns:og="http://opengraphprotocol.org/schema/" <?php language_attributes(); ?> class="no-js">
 
 <?php Front\tags()->head(); ?>
 
